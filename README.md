0b5d2759d5451e9843091d010b4bec6dd46d150e
Tiago Silva <tiago10moreira1@gmail.com>
2020-02-26 19:04:42 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Como Testar
+
+Use o comando `mvn package` no diretório principal e, após isso, será preciso utilizar o:
+
+  `java -classpath ./drivers/postgresql-42.2.8.jar:target/askMeAFlavor-1.0-SNAPSHOT.jar com.LP2.app.App`
+
+Isso ainda será melhorado, mas o projeto ainda está:
+
+## Em Andamento...
--- src/main/java/com/LP2/app/App.java
+++ src/main/java/com/LP2/app/App.java
@@ -2,22 +2,23 @@ package com.LP2.app;
 
 import com.LP2.app.loader.Loader;
 import com.LP2.database.Connect;
-// import com.LP2.view.terminal.routes.Router;
 
 public class App {
 
+  public static void printIt() {
+    System.out.println("Hello, World!");
+  }
+
+
   public static void main(String[] args) {
-    Connect db = null;
-    // Router router = null;
+    // Connect db = null;
 
-    // adicionar um loader de menu e all orders
-    Loader.preLoad();
-    db = new Connect();
-    // router = new Router();
-    Loader.loadControllers(db);
+    printIt();
+    // Loader.preLoad();
+    // db = new Connect();
+    // Loader.loadControllers(db);
 
-    // router.initialize();
 
-    Loader.unloadStreams();
+    // Loader.unloadStreams();
   }
 }
